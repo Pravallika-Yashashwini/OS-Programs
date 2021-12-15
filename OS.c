205bdce31a75df74c71b5712c190b475aa027b51
Pravallika Khandavilli <61104874+Pravallika-Yashashwini@users.noreply.github.com>
2021-12-15 19:35:09 +0530

    Update OS.c

--- OS.c
+++ OS.c
@@ -1,1 +1,248 @@
----FCFS---
+----------------------FCFS----------------------
+#include<stdio.h>
+  int main()
+ {
+    int n,bt[20],wt[20],tat[20],avwt=0,avtat=0,i,j;
+    printf("Enter total number of processes(maximum 20):");
+    scanf("%d",&n);
+     printf("nEnter Process Burst Timen");
+    for(i=0;i<n;i++)
+    {
+        printf("P[%d]:",i+1);
+        scanf("%d",&bt[i]);
+    }
+     wt[0]=0;   
+ 
+    for(i=1;i<n;i++)
+    {
+        wt[i]=0;
+        for(j=0;j<i;j++)
+            wt[i]+=bt[j];
+    }
+ 
+    printf("nProcessttBurst TimetWaiting TimetTurnaround Time");
+ 
+    for(i=0;i<n;i++)
+    {
+        tat[i]=bt[i]+wt[i];
+        avwt+=wt[i];
+        avtat+=tat[i];
+        printf("nP[%d]tt%dtt%dtt%d",i+1,bt[i],wt[i],tat[i]);
+    }
+ 
+    avwt/=i;
+    avtat/=i;
+    printf("nnAverage Waiting Time:%d",avwt);
+    printf("nAverage Turnaround Time:%d",avtat);
+ 
+    return 0;
+}
+---------------------------------------------------------------------
+  ----------SJF---------------
+  #include<stdio.h>
+ int main()
+{
+    int bt[20],p[20],wt[20],tat[20],i,j,n,total=0,pos,temp;
+    float avg_wt,avg_tat;
+    printf("Enter number of process:");
+    scanf("%d",&n);
+  
+    printf("nEnter Burst Time:n");
+    for(i=0;i<n;i++)
+    {
+        printf("p%d:",i+1);
+        scanf("%d",&bt[i]);
+        p[i]=i+1;         
+    }
+  
+   //sorting of burst times
+    for(i=0;i<n;i++)
+    {
+        pos=i;
+        for(j=i+1;j<n;j++)
+        {
+            if(bt[j]<bt[pos])
+                pos=j;
+        }
+  
+        temp=bt[i];
+        bt[i]=bt[pos];
+        bt[pos]=temp;
+  
+        temp=p[i];
+        p[i]=p[pos];
+        p[pos]=temp;
+    }
+   
+    wt[0]=0;            
+  
+   
+    for(i=1;i<n;i++)
+    {
+        wt[i]=0;
+        for(j=0;j<i;j++)
+            wt[i]+=bt[j];
+  
+        total+=wt[i];
+    }
+  
+    avg_wt=(float)total/n;      
+    total=0;
+  
+    printf("nProcesst    Burst Time    tWaiting TimetTurnaround Time");
+    for(i=0;i<n;i++)
+    {
+        tat[i]=bt[i]+wt[i];   
+        total+=tat[i];
+        printf("np%dtt  %dtt    %dttt%d",p[i],bt[i],wt[i],tat[i]);
+    }
+  
+    avg_tat=(float)total/n;    
+    printf("nnAverage Waiting Time=%f",avg_wt);
+    printf("nAverage Turnaround Time=%fn",avg_tat);
+}
+--------------------------------------------------------
+  -------------Priority----------------------
+  #include<stdio.h>
+ 
+int main()
+{
+    int bt[20],p[20],wt[20],tat[20],pr[20],i,j,n,total=0,pos,temp,avg_wt,avg_tat;
+    printf("Enter Total Number of Process:");
+    scanf("%d",&n);
+ 
+    printf("\nEnter Burst Time and Priority\n");
+    for(i=0;i<n;i++)
+    {
+        printf("\nP[%d]\n",i+1);
+        printf("Burst Time:");
+        scanf("%d",&bt[i]);
+        printf("Priority:");
+        scanf("%d",&pr[i]);
+        p[i]=i+1;           
+    }
+ 
+    for(i=0;i<n;i++)
+    {
+        pos=i;
+        for(j=i+1;j<n;j++)
+        {
+            if(pr[j]<pr[pos])
+                pos=j;
+        }
+ 
+        temp=pr[i];
+        pr[i]=pr[pos];
+        pr[pos]=temp;
+ 
+        temp=bt[i];
+        bt[i]=bt[pos];
+        bt[pos]=temp;
+ 
+        temp=p[i];
+        p[i]=p[pos];
+        p[pos]=temp;
+    }
+ 
+    wt[0]=0; 
+ 
+    for(i=1;i<n;i++)
+    {
+        wt[i]=0;
+        for(j=0;j<i;j++)
+            wt[i]+=bt[j];
+ 
+        total+=wt[i];
+    }
+ 
+    avg_wt=total/n;      
+    total=0;
+ 
+    printf("\nProcess\t    Burst Time    \tWaiting Time\tTurnaround Time");
+    for(i=0;i<n;i++)
+    {
+        tat[i]=bt[i]+wt[i];    
+        total+=tat[i];
+        printf("\nP[%d]\t\t  %d\t\t    %d\t\t\t%d",p[i],bt[i],wt[i],tat[i]);
+    }
+ 
+    avg_tat=total/n;     
+    printf("\n\nAverage Waiting Time=%d",avg_wt);
+    printf("\nAverage Turnaround Time=%d\n",avg_tat);
+ 
+return 0;
+}
+-----------------Round Robins--------------
+ #include<stdio.h>
+main()
+
+{
+
+int i,j,n,bu[10],wa[10],tat[10],t,ct[10],max;
+
+float awt=0,att=0,temp=0;
+
+printf("Enter the no of processes -- ");
+
+scanf("%d",&n);
+
+for(i=0;i<n;i++)
+
+{
+
+printf("\nEnter Burst Time for process %d -- ", i+1);
+scanf("%d",&bu[i]);
+
+ct[i]=bu[i];
+
+}
+
+printf("\nEnter the size of time slice -- ");
+scanf("%d",&t);
+
+max=bu[0];
+
+for(i=1;i<n;i++)
+if(max<bu[i])
+
+max=bu[i];
+
+for(j=0;j<(max/t)+1;j++)
+
+for(i=0;i<n;i++)
+if(bu[i]!=0)
+
+if(bu[i]<=t)
+
+{
+
+tat[i]=temp+bu[i];
+temp=temp+bu[i];
+
+bu[i]=0;
+
+}
+else
+{
+
+bu[i]=bu[i]-t;
+
+temp=temp+t;
+}
+
+for(i=0;i<n;i++)
+
+{
+
+wa[i]=tat[i]-ct[i];
+att+=tat[i];
+
+	awt+=wa[i];		
+}		
+	printf("\nThe Average Turnaround time is -- %f",att/n);
+	printf("\nThe Average Waiting time is -- %f ",awt/n);
+	printf("\n\tPROCESS\t BURST TIME \t WAITING TIME\tTURNAROUND TIME\n");
+	for(i=0;i<n;i++)		
+	printf("\t%d \t %d \t\t %d \t\t %d \n",i+1,ct[i],wa[i],tat[i]);
+	
+}			
